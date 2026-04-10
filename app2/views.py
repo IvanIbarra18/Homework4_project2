50b8c901570debd00170f043d80ac2e8b3035274
IvanIbarra18 <ibarrajaimesjesusivan3@gmail.com>
2026-04-10 21:04:48 +0000

    Project2_done

---
+++ app2/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'app2/index.html')
+
+def maison(request):
+    return render(request, 'app2/maison-templatemo.html')
